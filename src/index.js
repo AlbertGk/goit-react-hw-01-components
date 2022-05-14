4f83ac3b8e9072b9314302344a332a7758964825
AlbertGk <albertgk01@gmail.com>
2022-05-15 01:03:09 +0200

    task1 update

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { Profile } from 'components/Profile';
+import user from 'user.json';
+
+import { Statistics } from 'components/Statistics';
+import data from 'data.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+
+
+    </App>
   </React.StrictMode>
 );
